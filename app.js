b0df06488fef22d8a19a3289e7d0d608dca57901
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2025-06-30 23:12:00 +0100

    Refactor stress test to handle multiple URLs and enhance result logging; update notes for clarity

--- app.js
+++ app.js
@@ -5,7 +5,18 @@ const app = express();
 app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
+    for (let i = 0; i < 1000000000; i++) {
+        // Simulating some CPU work
+    }
+
     res.send('Hello, World!');
+});
+
+app.get('/health', (req, res) => {
+    for (let i = 0; i < 1000000000; i++) {
+        // Simulating some CPU work for health check
+    }
+    res.send("health check passed");
 });
 
 app.listen(3000, () => {
--- stress_test/test.js
+++ stress_test/test.js
@@ -1,28 +1,42 @@
 const autocannon = require('autocannon');
 
-const url = 'http://localhost:3000'; // Change this to your server URL
+const urls = [
+    'http://localhost:3000',
+    'http://localhost:3000/health',
+]
 const duration = 30; // Duration of the test in seconds
 
-const instance = autocannon({
-    url,
-    duration,
-    // connections: 100, // Number of concurrent connections
-    // pipelining: 1, // Number of requests per connection
-    // headers: {
-    //     'User-Agent': 'Autocannon Test'
-    // }
-}, (err, result) => {
-    if (err) {
-        console.error('Error:', err);
-        // process.exit(1);
-    }
-    else {
-        console.log('Results:', result);
-    }
-    // console.log(autocannon.printResult(result));
-});
+urls.forEach(url => {
+    const instance = autocannon({
+        url,
+        duration,
+        // connections: 100, // Number of concurrent connections
+        // pipelining: 1, // Number of requests per connection
+        // headers: {
+        //     'User-Agent': 'Autocannon Test'
+        // }
+    }, (err, result) => {
+        if (err) {
+            console.error('Error:', err);
+            // process.exit(1);
+        }
+        else {
+            // console.log('Results:', result);
+            console.log(`Results for ${url}:`);
+            console.log('Number of requests:', result.requests.total);
+            console.log('Total time:', result.duration, 'ms');
+            console.log('Mean latency:', result.latency.mean, 'ms');
+            console.log('Requests per second:', result.requests.average);
+        }
+        // console.log(autocannon.printResult(result));
+    });
 
-autocannon.track(instance);
+    autocannon.track(instance, {
+        renderProgressBar: false,
+        renderResultsTable: false,
+        renderLatencyTable: false
+    });
+});
 
 // autocannon({
 //     url: 'http://localhost:3000',
